81235279b38439443b5cafbe14a8938a7ef9ed51
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-05-28 13:53:56 +0200

    Projektdateien hinzufügen.

---
+++ M120_226B_UTT_Project/GameField.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M120_226B_UTT_Project {
+    /// <summary>
+    /// Interaktionslogik für GameField.xaml
+    /// </summary>
+    public partial class GameField : UserControl {
+        public GameField() {
+            InitializeComponent();
+        }
+        public void setFieldToX() {
+            //imgButtonImage.Source
+        }
+    }
+}
---
+++ M120_226B_UTT_Project/GameSetupWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M120_226B_UTT_Project {
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class GameSetupWindow : Window {
+        public GameSetupWindow() {
+            InitializeComponent();
+        }
+    }
+}
